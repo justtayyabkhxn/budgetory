548d2b66ebad8bbddd4974d0c55ba17267403a0f
Tayyab Khan <tayyabkhangk4734@gmail.com>
2025-05-06 15:42:34 +0530

    deploy new

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { AddTransactionForm } from "../../components/AddTransactionForm";
-import { TxnCard } from "../../components/TxnCard";
 import Link from "next/link";
 
 interface Transaction {
